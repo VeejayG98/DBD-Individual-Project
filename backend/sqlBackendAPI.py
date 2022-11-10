259662e2c4c482c8ed4603f0d56a34600b3486c5
jayasuryaa-govindraj <jayasuryaa1998@gmail.com>
2022-11-10 11:59:13 -0600

    Modified the getBooks API to display the books correctly. Added code to check if Card_ID is present in the system or not in checkoutBook API. Added the Allow CORS option into Flask.

--- backend/sqlBackendAPI.py
+++ backend/sqlBackendAPI.py
@@ -3,6 +3,7 @@ from datetime import datetime, timedelta
 from matplotlib.style import available
 import mysql.connector
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 import pandas as pd
 
 
@@ -20,6 +21,7 @@ def connectSQLDB():
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
 
 @app.route('/books/all', methods=['GET'])
@@ -30,13 +32,23 @@ def getBooks():
 
     mycursor.execute("SELECT AUTHORS.NAME, BOOKS.TITLE, BOOKS.ISBN13, BOOKS.AVAILABLE FROM \
     AUTHORS INNER JOIN BOOK_AUTHORS ON AUTHORS.AUTHOR_ID = BOOK_AUTHORS.AUTHOR_ID \
-    INNER JOIN BOOKS ON BOOK_AUTHORS.ISBN13 = BOOKS.ISBN13")
+    INNER JOIN BOOKS ON BOOK_AUTHORS.ISBN13 = BOOKS.ISBN13 \
+    ORDER BY BOOKS.ISBN13 DESC")
 
     myresult = mycursor.fetchall()
+    myresult = pd.DataFrame(myresult)
+    agg_functions = {'NAME': lambda x: ", ".join(x),
+                    'TITLE': 'first',
+                    'AVAILABLE': 'first'}
+
+    myresult = myresult.groupby(
+        'ISBN13', as_index=False).agg(agg_functions)
+    myresult = myresult.to_dict('records')
+
     mycursor.close()
     mydb.close()
 
-    return jsonify(myresult)
+    return jsonify(myresult), 200
 
 
 @app.route('/books/search', methods=['GET'])
@@ -83,13 +95,22 @@ def checkoutBook():
     date_out = datetime.now().date()
     due_date = date_out + timedelta(days=7)
 
+
     mydb = connectSQLDB()
     mycursor = mydb.cursor(dictionary=True)
+
+    mycursor.execute("SELECT CARD_ID FROM BORROWERS")
+    # print(mycursor.fetchall())
+    card_ids = set([i['CARD_ID'] for i in mycursor.fetchall()])
+
+    if card_id not in card_ids:
+        return "", "400 Card Number not present in the system!"
+
 
     mycursor.execute(f"SELECT AVAILABLE FROM BOOKS WHERE ISBN13 = '{isbn13}'")
     available = mycursor.fetchall()[0]['AVAILABLE']
     if available == 0:
-        return "Book is already checked out!"
+        return "", "400 Book is already checked out!"
 
     mycursor.execute(
         f"SELECT COUNT(*) FROM BOOK_LOANS WHERE CARD_ID = '{card_id}' AND DATE_IN IS NULL")
@@ -97,7 +118,7 @@ def checkoutBook():
     print(no_of_books, type(no_of_books))
 
     if no_of_books == 3:
-        return "User has borrowed more than 3 books!"
+        return "", "400 User has borrowed more than 3 books!"
 
     mycursor.execute("INSERT INTO BOOK_LOANS (ISBN13, CARD_ID, DATE_OUT, DUE_DATE) VALUES (%s, %s, %s, %s)",
                      (isbn13, card_id, date_out, due_date))
@@ -109,7 +130,7 @@ def checkoutBook():
     mycursor.close()
     mydb.close()
 
-    return 'OK'
+    return 'OK', 200
 
 
 @app.route('/books/checkin', methods=['POST'])
