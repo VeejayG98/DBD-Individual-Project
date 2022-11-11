54b4609fa983e132cc3ef19cca30d982fefe2812
jayasuryaa-govindraj <jayasuryaa1998@gmail.com>
2022-11-11 13:52:30 -0600

    Created Routes for the web app.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,77 +1,11 @@
-import { useEffect, useState } from "react";
-import { TextField } from "@mui/material";
-import { Modal } from "@mui/material";
-import { Box } from "@mui/material";
-import { Typography } from "@mui/material";
-import { Button } from "@mui/material";
-import BasicTable from "./components/BasicTable";
-import NavBar from "./components/Navbar";
-import CheckoutModal from "./components/CheckoutModal";
+import { Routes, Route, useNavigate } from 'react-router-dom';
+import Home from './Home';
 
-
-function App() {
-  const [books, setBooks] = useState([]);
-  const [modalOpen, setModalOpen] = useState(false);
-  const [ISBN13, setISBN] = useState("");
-
-  const handleOpen = (event) => {
-    setModalOpen(true);
-    setISBN(event.target.id);
-  };
-  const handleClose = () => {
-    setModalOpen(false);
-  };
-
-  function handleSearch(event) {
-    fetch(`http://127.0.0.1:5000/books/search?search=${event.target.value}`, {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data.splice(0, 50));
-      });
-  }
-
-
-  useEffect(() => {
-    fetch("http://127.0.0.1:5000/books/all", {
-      headers: {
-        "Content-Type": "application/json",
-        Accept: "application/json",
-      },
-    })
-      .then((res) => {
-        console.log(res);
-        return res;
-      })
-      .then((res) => res.json())
-      .then((data) => {
-        setBooks(data.splice(0, 50));
-      });
-  }, []);
-
-  useEffect(() => {
-    console.log(books.slice(0, 10));
-  }, [books]);
-
-  return (
-    <div>
-      <NavBar/>
-      <TextField
-        id="filled-basic"
-        label="Search Books"
-        variant="filled"
-        onChange={handleSearch}
-      />
-      <BasicTable books={books} handleOpen={handleOpen} />
-      <CheckoutModal modalOpen={modalOpen} handleClose={handleClose} ISBN13={ISBN13} />
-    </div>
-  );
+function App(){
+    return(
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+      </Routes>  
+    );
 }
-
-
-
 export default App;
---
+++ frontend/src/Home.js
@@ -0,0 +1,72 @@
+import { useEffect, useState } from "react";
+import { TextField } from "@mui/material";
+import BasicTable from "./components/BasicTable";
+import NavBar from "./components/Navbar";
+import CheckoutModal from "./components/CheckoutModal";
+
+function Home() {
+  const [books, setBooks] = useState([]);
+  const [modalOpen, setModalOpen] = useState(false);
+  const [ISBN13, setISBN] = useState("");
+
+  const handleOpen = (event) => {
+    setModalOpen(true);
+    setISBN(event.target.id);
+  };
+  const handleClose = () => {
+    setModalOpen(false);
+  };
+
+  function handleSearch(event) {
+    fetch(`http://127.0.0.1:5000/books/search?search=${event.target.value}`, {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data.splice(0, 50));
+      });
+  }
+
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:5000/books/all", {
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => {
+        console.log(res);
+        return res;
+      })
+      .then((res) => res.json())
+      .then((data) => {
+        setBooks(data.splice(0, 50));
+      });
+  }, []);
+
+  useEffect(() => {
+    console.log(books.slice(0, 10));
+  }, [books]);
+
+  return (
+    <div>
+      <NavBar/>
+      <TextField
+        id="filled-basic"
+        label="Search Books"
+        variant="filled"
+        onChange={handleSearch}
+      />
+      <BasicTable books={books} handleOpen={handleOpen} />
+      <CheckoutModal modalOpen={modalOpen} handleClose={handleClose} ISBN13={ISBN13} />
+    </div>
+  );
+}
+
+
+
+export default Home;
