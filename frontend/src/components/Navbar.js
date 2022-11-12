640744196ccc7263723f3e001a1ab0c408814f07
jayasuryaa-govindraj <jayasuryaa1998@gmail.com>
2022-11-11 18:46:53 -0600

    Added navigation links in Navbar for home and checkin

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,19 +1,23 @@
 // import './App.css';
 import { AppBar, Toolbar, IconButton, Typography, Stack, Button } from '@mui/material';
 import BookIcon from '@mui/icons-material/Book';
+import {useNavigate} from "react-router-dom";
 
 function NavBar() {
+
+  const navigate = useNavigate();
+
   return(
     <AppBar position='static'>
       <Toolbar>
-        <IconButton size='large' edge='start' color='inherit' aria-label='logo'>
+        <IconButton size='large' edge='start' color='inherit' aria-label='logo' onClick={() => navigate('/')}>
           <BookIcon fontSize='large'/>
         </IconButton>
         <Typography variant='h6' component='div' sx={{ flexGrow: 1 }}>
           UTD Library Management
         </Typography>
         <Stack direction='row' spacing={2} >
-          <Button color='inherit'>Check in Book</Button>
+          <Button color='inherit' onClick={() => navigate('/checkin')}>Check in Book</Button>
           <Button color='inherit'>Pay Fines</Button>
           <Button color='inherit'>View Fines</Button>
           <Button color='inherit'>Add New Borrower</Button>
