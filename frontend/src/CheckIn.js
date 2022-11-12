de1ded1be2d0fa4ed70e633818f7a29f44939356
jayasuryaa-govindraj <jayasuryaa1998@gmail.com>
2022-11-11 18:57:37 -0600

    Added search functionality in Book loans.

--- frontend/src/CheckIn.js
+++ frontend/src/CheckIn.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import LoansTable from "./components/LoansTable";
+import { TextField } from "@mui/material";
 import NavBar from "./components/Navbar";
 
 function CheckIn() {
@@ -20,9 +21,32 @@ function CheckIn() {
     console.log(loans);
   }, [loans]);
 
+  function handleSearch(event) {
+    fetch(`http://127.0.0.1:5000/loans/search`, {
+      method: "POST",
+      body: JSON.stringify({
+        search: event.target.value
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setLoans(data);
+      });
+  }
+
   return (
     <div>
       <NavBar />
+      <TextField
+        id="filled-basic"
+        label="Search Books Loans"
+        variant="filled"
+        onChange={handleSearch}
+      />
       <div style={{ margin: "20px" }}>
         <LoansTable loans={loans} />
       </div>
